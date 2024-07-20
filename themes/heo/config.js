7c6143302b4b2b1f9b9d05d18cad7e1636f98895
Lang123646 <72953864+Lang123646@users.noreply.github.com>
2024-07-20 17:31:44 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -15,8 +15,8 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '勿',
-  HEO_HERO_TITLE_2: '忘我',
+  HEO_HERO_TITLE_1: ' ',
+  HEO_HERO_TITLE_2: '勿忘我',
   HEO_HERO_TITLE_3: 'leooel.xyz',
   HEO_HERO_TITLE_4: '',
   HEO_HERO_TITLE_5: '今天也是摆烂的一天呢',
